2002f83079857a71c875ffa4ff91a3edc43284b0
ochilovshahzod <shahzodochilov73@gmail.com>
2024-05-31 13:52:37 +0500

    bajarildi

---
+++ android/app/src/main/kotlin/com/example/mustaqil_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mustaqil_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:mustaqil_2/hisobla.dart';
+void main() => runApp(MaterialApp(
+  theme: ThemeData(
+    primarySwatch: Colors.red,
+  ),
+  home: HomePage(),
+));
